ef4fd0e350d896d44aea080a289342ba8657cf38
aziz zaaine <zaaine@live.fr>
2024-08-25 10:15:42 +0200

    correction error extension + gestion de la déconnexion avec redirect sur parge d'accueil.

--- Frontend/logout.php
+++ Frontend/logout.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once(__DIR__ . 'functions.php');
+require_once(__DIR__ . '/modules/functions.php');
 
 // detruire la session
 session_unset();
--- Frontend/modules/header.php
+++ Frontend/modules/header.php
@@ -1,42 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>header</title>
-</head>
-
-<body>
-    <!-- header.php -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Site de recettes</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Site de recettes</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <?php if (isset($_SESSION['LOGGED_USER'])) : ?>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link" href="logout.php">Déconnexion</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
-                    </li>
-
-                    <!-- Afficher le lien Logout si l'utilisateur est connecté -->
-                    <?php if (!isset($_SESSION['LOGGED_USER'])) : ?>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Logout</a>
-                        </li>
-
-                    <?php endif; ?>
-
-                </ul>
-            </div>
+                <?php endif; ?>
+            </ul>
         </div>
-    </nav>
-</body>
-
-</html>
+    </div>
+</nav>
